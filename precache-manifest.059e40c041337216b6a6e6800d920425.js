bc113907cbe379ed7533181d81c4e9ddcfe2d036
skpulipaka26 <skpulipaka@gmail.com>
2019-01-18 18:33:20 -0800

    Updates

--- precache-manifest.059e40c041337216b6a6e6800d920425.js
+++ precache-manifest.059e40c041337216b6a6e6800d920425.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/area-books/static/js/runtime~main.9d4e46a0.js"
   },
   {
-    "revision": "1ad490d80a405eefa5ab",
-    "url": "/area-books/static/js/main.1ad490d8.chunk.js"
+    "revision": "a14a2d146d3bd80f2bf5",
+    "url": "/area-books/static/js/main.a14a2d14.chunk.js"
   },
   {
     "revision": "74a81af2b0e207141586",
     "url": "/area-books/static/js/1.74a81af2.chunk.js"
   },
   {
-    "revision": "1ad490d80a405eefa5ab",
+    "revision": "a14a2d146d3bd80f2bf5",
     "url": "/area-books/static/css/main.edd714fe.chunk.css"
   },
   {
-    "revision": "5f25e9557429404af09251e96eadb1d7",
+    "revision": "48c68483000cb4dcf1c1021180f486e8",
     "url": "/area-books/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/area-books/precache-manifest.0b1a8173a43b364173091d6b32b142de.js"
+  "/area-books/precache-manifest.059e40c041337216b6a6e6800d920425.js"
 );
 
 workbox.clientsClaim();
